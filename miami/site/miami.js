c38cfe1cecb404b2e89b4f87abe61ac5fccd0a66
Billy Jattin <bjattin@veteranmediagroup.com>
2024-09-26 21:18:37 -0400

    Started making the templates dynamic

--- miami/site/miami.js
+++ miami/site/miami.js
@@ -1,7 +1,19 @@
+// Sections descriptions:
+
+// About: 1 short paragraph per page
+
+// Services: Show at least 4 services per page
+
+// Packages: Show at least 6 packages per page
+
+// Gallery: Show at least 16 images per page 
+
 // imports express into our project 
 const express = require('express') 
 //create the express server inside a variable called app
 const app = express()
+//Specify static routes
+app.use(express.static('public'))
 // import a package for handlebars
 const expressHandlebars = require('express-handlebars')
 // make express use the handlebars template engine
@@ -13,15 +25,28 @@ app.set('view engine','handlebars')
 const PORT = process.env.port || 3000
 //process routes before error
 app.get('/',(request,response)=>{
-    response.render('home')
+    response.render('landing',{
+        title:"This is Miami!!",
+        abstract:"Miami is a great place to live.",
+        image:"miamisky.jpg"
+    })
 })
 app.get('/about',(request,response)=>{
-    response.render('about')
+    response.render('page',{
+        title:"About Miami",
+        abstract:"From the Miami Marlins to the Miami Heat, there's a lot to see. "
+    })
 })
+
 app.get('/nightlife',(request,response)=>{
-    response.type('text/plain')
-    response.send('Miami At Night')
+    response.render('page',{
+        title:"Miami at Night",
+        abstract:"Stay away from South Beach!! "
+    })
 })
+
+
+
 app.get('/beaches',(request,response)=>{
     response.type('text/plain')
     response.send('Miami Beach and more!')
