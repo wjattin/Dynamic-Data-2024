2d5007efcd8154ffd12e7b2895363dfd9bd0409b
Billy Jattin <bjattin@veteranmediagroup.com>
2024-11-14 17:23:34 -0500

    madlibs

---
+++ madlibs/app.js
@@ -0,0 +1,60 @@
+// npm install express 
+// npm install express-handlebars 
+// sudo npm install express 
+// sudo npm install express-handlebars 
+//Initialize Express
+const express = require('express')
+
+//add the handlebars view engine 
+const expressHandlebars = require('express-handlebars') 
+
+const app = express()
+
+//configure our express app to use handlebars
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',
+}))
+
+app.set('view engine','handlebars')
+//ends handlebar configuration
+
+const port = process.env.port || 3000
+app.get("/",(req,res)=>{
+    res.render('page',{req})
+})
+
+app.get("/mad",(req,res)=>{
+    const data = require("./data/mad-data.json")
+    res.render('madform',{data})
+})
+
+
+
+app.post('/process',(req,res)=>{
+    res.send('got post')
+})
+
+app.get('/process',(req,res)=>{
+    console.log(req.query)
+})
+
+
+//Error handling ->  app.use() basic express route 
+app.use((req,res) => {
+    res.status(404)
+    res.render('404')
+})
+
+//Server Error 500
+app.use((error,req,res,next) => {
+    console.log(error.message)
+    res.status(500)
+    res.render('500') 
+}) 
+
+// setup listener
+app.listen(port,()=>{
+    console.log(`Server started http://localhost:${port}`)
+    //console.log('Server starter http://localhost:'+port)
+    console.log('To close pres Ctrl-C')
+})
